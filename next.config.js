85abc7afcff0b887d4f0af7f4de167027c8909da
Clint Kolodziej <clint@clintkolodziej.com>
2019-09-06 16:31:43 -0500

    Clean up exportPathMap in next.config.js

--- next.config.js
+++ next.config.js
@@ -2,57 +2,83 @@ const path = require('path');
 const fs = require('fs');
 
 module.exports = {
+
     webpack: (config) => {
+
         config.module.rules.push(
             {
                 test: /\.md$/,
                 use: 'raw-loader'
             }
         )
+
         return config;
+
     },
 
     exportPathMap: async function() {
+
+        //
+        // Set up static paths
+        //
 
         const paths = {
           '/': { page: '/' },
           //'/about': { page: '/about' }
         };
 
-        const directoryPath = path.join(__dirname, 'content');
+        //
+        // Set up dynamic paths: post/[pid]
+        //
 
-        let mdFiles = fs.readdirSync(directoryPath).filter(file => {
-            return file.match(/\.md$/);
-        });
+        let posts = getPosts();
 
-        console.log("");
-        console.log("Markdown files in 'content' directory: ");
-        console.log(mdFiles);
-        console.log("");
+        logWithDescription(posts, "Registered content slugs for posts");
 
-        let posts = mdFiles.map((key, index) => {
-
-            // Create slug from filename
-            const slug = key.replace(/^.*[\\\/]/, '').split('.').slice(0, -1).join('.');
-
-            return {
-                slug
-            };
-
-        });
-
-        console.log("Registered content slugs for posts: ");
-        console.log(posts);
-        console.log("");
-    
         posts.forEach(post => {
           paths[`/post/${post.slug}`] = { page: '/post/[pid]', query: { pid: post.slug } };
         });
 
-        console.log("Paths being registered: ");
-        console.log(paths);
-        console.log("");
+        //
+        // Return the resulting path list
+        //
+
+        logWithDescription(paths, "Paths being registered");
 
         return paths;
-      }
+
+    }
+}
+
+function getPosts() {
+
+    const directoryPath = path.join(__dirname, 'content');
+
+    let mdFiles = fs.readdirSync(directoryPath).filter(file => {
+        return file.match(/\.md$/);
+    });
+
+    logWithDescription(mdFiles, "Markdown files in 'content' directory");
+
+    let posts = mdFiles.map((key, index) => {
+
+        // Create slug from filename
+        const slug = key.replace(/^.*[\\\/]/, '').split('.').slice(0, -1).join('.');
+
+        return {
+            slug
+        };
+
+    });
+
+    return posts;
+
+}
+
+function logWithDescription(obj, description) {
+
+    console.log("");
+    console.log(description + ":");
+    console.log(obj);
+
 }
